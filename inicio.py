885c9002585b56cd50bbf7b30c0e942d0ff6452a
Angysg <angysg8@gmail.com>
2024-10-07 17:39:05 +0200

    primer commit

---
+++ inicio.py
@@ -0,0 +1,11 @@
+a=input("¿Cómo te llamas?")
+print("Hola " + a)
+print(f"Hola {a}")  #Para concatenar tambien se usa la f.
+
+for i in range(0,10):
+    print(i)
+#El fin del bucle no es con llaves, si no con tabulador, es decir:
+    #print(i) Este entra dentro del bucle y lo pinta 10 veces.
+#print(i). Aqui esta fuera del bucle y se escribe solo una vez después del bucle.
+'Esto tambien es comentario'
+
---
+++ len.py
@@ -0,0 +1,6 @@
+a=7
+nombre ="Pepe"
+print(type(a))
+print("uno", "dos", len(nombre), "tres", a, sep="*", end="--")
+
+#len es para que te de la longitud del String.
