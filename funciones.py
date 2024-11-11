fda1bab41d7ff195bceb47ad3e4b92e1a141bc45
Angysg <angysg8@gmail.com>
2024-11-11 17:48:49 +0100

    funciones y listas

---
+++ funciones.py
@@ -0,0 +1,19 @@
+def unaFuncion():
+    pass 
+    #Esta palabra está reservada para que se desarrolle la función más adelante y no de fallo. 
+    #Se puede dejar sin código, aunque no tenga llaves.
+
+def otra():
+    for i in range(2,100,2): #Numeros pares.
+        pass
+
+
+lista= [5, 4, 3]
+lista.append(7) #Añadir a la lista el 7
+print(lista)
+
+tupla=(1,2,3,2,8)
+print(tupla.count(2))
+print(tupla.index(4))
+
+
